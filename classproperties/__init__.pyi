fb14c8ccdc63e91ae6422a810840519e88b45de5
CarrotManMatt <matt@carrotmanmatt.com>
2024-01-05 22:12:02 +0000

    Delete classproperties/__init__.pyi

--- classproperties/__init__.pyi
+++
@@ -1,8 +0,0 @@
-from collections.abc import Callable
-from typing import Any, Generic, TypeVar
-
-T = TypeVar('T')
-
-class classproperty(property, Generic[T]):
-    def __init__(self, func: Callable[[Any], T]) -> None: ...
-    def __get__(self, owner_self: object, owner_cls: type | None = ...) -> T: ...
